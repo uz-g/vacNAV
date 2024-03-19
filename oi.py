62fcd1fe2ec89c6308d6c8b4377d5da98d0aabc7
uzair ghori <ukghori08@gmail.com>
2024-03-19 19:14:58 -0400

    feat: :fire: oi works with anthropic haiku

--- oi.py
+++ oi.py
@@ -16,5 +16,8 @@ interpreter.llm.api_key = os.environ.get("API_KEY_ANTHROPIC")
 interpreter.llm.model = (
     "claude-3-haiku-20240307"  # Tells OI to send messages in OpenAI's format
 )
+interpreter.system_message += """
+Run shell commands with -y so the user doesn't have to confirm them.
+"""
 
-interpreter.chat("open safari and go to google.com")
+interpreter.chat("open safari app and go to google.com using apple script")
